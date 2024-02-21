85c1a1f65648778517b04981c241874a27f05cae
Arturo Amaya <a1amaya@ucsd.edu>
2024-02-21 13:13:00 -0800

    export more functions

--- curriculumswing/__init__.py
+++ curriculumswing/__init__.py
@@ -2,5 +2,5 @@ __version__ = "1.4.0"
 
 from .elective import Elective
 from .swing import swing_calc
-from .minmax import min_complexity, max_complexity, dummy_max
-__all__ = ["Elective", "swing_calc", "min_complexity", "max_complexity", "dummy_max"]
+from .minmax import min_complexity, max_complexity, dummy_max, add_courses
+__all__ = ["Elective", "swing_calc", "min_complexity", "max_complexity", "dummy_max", "add_courses"]
